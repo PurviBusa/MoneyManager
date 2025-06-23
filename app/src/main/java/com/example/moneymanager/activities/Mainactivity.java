aaa1852eb66d3fa712813edad93a354c2425f3f2
admin <purvibusa5@gmail.com>
2025-06-23 18:09:19 +0530

    test

--- app/src/main/java/com/example/moneymanager/activities/Mainactivity.java
+++ app/src/main/java/com/example/moneymanager/activities/Mainactivity.java
@@ -12,7 +12,10 @@ public class Mainactivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initViews();
+    }
 
-
+    private void initViews() {
     }
 }
