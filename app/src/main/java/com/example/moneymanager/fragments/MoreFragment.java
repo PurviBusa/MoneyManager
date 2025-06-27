8a2f5851b0e3c79d99df92424350d9d637625344
admin <purvibusa5@gmail.com>
2025-06-27 14:06:40 +0530

    update

--- app/src/main/java/com/example/moneymanager/fragments/MoreFragment.java
+++ app/src/main/java/com/example/moneymanager/fragments/MoreFragment.java
@@ -7,11 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.moneymanager.R;
 
 
 public class MoreFragment extends Fragment {
+    TextView configuration;
 
 
     public MoreFragment() {
